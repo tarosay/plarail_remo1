5e82a3f2f13cd4ee4a26aca80fc8480f42b99e35
Tarosa <tarosa.y@gmail.com>
2025-10-19 05:23:17 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+let 速さ = 0
+let 回 = 0
 let Group = 0
 if (input.buttonIsPressed(Button.A)) {
     Group = 3
@@ -8,5 +10,21 @@ if (input.buttonIsPressed(Button.A)) {
 }
 radio.sendValue("c", 0)
 basic.forever(function () {
-    basic.showNumber(input.acceleration(Dimension.Z))
+    if (input.acceleration(Dimension.Y) > 2000) {
+        回 = 回 + 1
+    } else {
+        回 = 回 * 0.99
+    }
+    速さ = 回 / 35
+    if (速さ > 1) {
+        速さ = 1023
+    } else {
+        速さ = 回 * 1023
+        radio.sendValue("c", 速さ)
+    }
+    if (回 > 30) {
+        basic.showArrow(ArrowNames.North)
+    } else {
+        basic.clearScreen()
+    }
 })

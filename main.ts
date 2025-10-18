b9b13e766a1fe8cebf528d8ba73956c450654adf
Tarosa <tarosa.y@gmail.com>
2025-10-19 04:50:02 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,12 @@
-let 速さ = 0
-radio.setGroup(5)
+let Group = 0
+if (input.buttonIsPressed(Button.A)) {
+    Group = 3
+} else if (input.buttonIsPressed(Button.B)) {
+    Group = 4
+} else {
+	
+}
 radio.sendValue("c", 0)
 basic.forever(function () {
-    速さ = input.acceleration(Dimension.Y) / 1023 * -100
-    if (input.buttonIsPressed(Button.AB)) {
-        radio.sendValue("c", 速さ)
-        if (速さ > 0) {
-            basic.showArrow(ArrowNames.North)
-        } else if (速さ < 0) {
-            basic.showArrow(ArrowNames.South)
-        } else {
-            basic.showIcon(IconNames.SmallDiamond)
-        }
-    } else if (input.buttonIsPressed(Button.A)) {
-        radio.sendValue("a", 速さ)
-        basic.showArrow(ArrowNames.West)
-    } else if (input.buttonIsPressed(Button.B)) {
-        radio.sendValue("b", 速さ)
-        basic.showArrow(ArrowNames.East)
-    } else {
-        radio.sendValue("c", 0)
-        basic.showIcon(IconNames.SmallDiamond)
-    }
+    basic.showNumber(input.acceleration(Dimension.Z))
 })

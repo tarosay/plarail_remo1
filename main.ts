513d04a6e9059f32b62512d47f171b6be5d80f6b
Tarosa <tarosa.y@gmail.com>
2025-10-19 06:21:04 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,31 @@
 let 速さ = 0
-let 回 = 0
 if (input.buttonIsPressed(Button.A)) {
     radio.setGroup(3)
 } else if (input.buttonIsPressed(Button.B)) {
     radio.setGroup(4)
 } else {
-    radio.setGroup(5)
+    radio.setGroup(4)
 }
 radio.sendNumber(0)
+let 回 = 0
 basic.forever(function () {
     if (input.acceleration(Dimension.Y) > 2000) {
         回 = 回 + 1
     } else {
-        回 = 回 * 0.99
+        回 = 回 * 0.9
     }
-    if (回 > 36) {
-        回 = 36
+    if (回 > 10) {
+        回 = 10
+    } else if (回 <= 0) {
+        回 = 0
+    } else {
+    	
     }
-    速さ = Math.trunc(回 / 36 * 100)
+    速さ = Math.trunc(回 / 10 * 100)
     radio.sendNumber(速さ)
-    if (回 > 18) {
+    if (回 >= 5) {
         basic.showArrow(ArrowNames.North)
     } else {
-        basic.showIcon(IconNames.Chessboard)
+        basic.clearScreen()
     }
 })
